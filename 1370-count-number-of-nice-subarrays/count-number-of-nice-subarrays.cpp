10816e85ea3ed69e0eb2ea9e74257a9b14ec69a1
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2024-09-17 23:32:33 +0530

    Time: 92 ms (66.83%) | Memory: 70.1 MB (97.91%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return function(nums, k) - function(nums, k - 1);
+    }
+
+private:
+    int function(const vector<int>& nums, int k) {
+        int l = 0, r = 0, max_odd = 0, n = nums.size();
+        int ans = 0;
+
+        while (r < n) {
+            if (isodd(nums[r])) {
+                max_odd++;
+            }
+
+            while (max_odd > k) {
+                if (isodd(nums[l])) {
+                    max_odd--;
+                }
+                l++;
+            }
+
+            ans += (r - l + 1);
+            r++;
+        }
+
+        return ans;
+    }
+
+    bool isodd(int n) {
+        if ((n ^ 1) == (n + 1))
+            return false;
+        else
+            return true;
+    }
+};
