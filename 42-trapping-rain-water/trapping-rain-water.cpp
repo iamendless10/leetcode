ef6664225ce3ad275a8b378a8fd265c72e1e0965
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2024-10-10 22:08:42 +0530

    Time: 20 ms (10.69%) | Memory: 27.4 MB (5.42%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        int res = 0;
-        if (n==0) return 0;
-        int l = 0,r = n-1;
-        int maxl =height[l] , maxr = height[r];
+    int trap(vector<int>& nums) {
+        vector<int> l;
+        vector<int> r;
 
-        while (l<r){
-            if (maxl < maxr){
-                l += 1;
-                maxl = max(maxl,height[l]);
-                res += maxl - height[l];
-            }
-            else{
-                r-=1;
-                maxr = max(maxr,height[r]);
-                res += maxr - height[r];
-            }
+        int maxl = 0;
+        for(int ele: nums){
+            maxl = max(maxl ,  ele);
+            l.push_back(maxl);
         }
-        return res;
         
+
+
+        int maxr = 0;
+        int n  = nums.size(); 
+        for(int i = n - 1 ; i >= 0 ; i--){
+            maxr = max(nums[i] , maxr);
+            r.push_back(maxr);
+        }
+        reverse(r.begin(),r.end());
+
+        int sum = 0,pot = 0;
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            pot = min(l[i], r[i]);
+            ans = pot - nums[i];
+            if(ans > 0)sum += ans;
+        }
+
+
+        return sum;
     }
+    
 };
