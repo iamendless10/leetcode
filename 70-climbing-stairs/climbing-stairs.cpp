c3b5da466b8b44533b573b2978efe92ea00c4285
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2024-10-19 10:29:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (36.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,8 +3,10 @@ public:
     int climbStairs(int n) {
         if (n <= 2) return n;
 
-        vector<int> dp(n + 1, -1);
-        return helper(n, dp);
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        return helper(n+1, dp);
     }
 
     int helper(int n, vector<int> &dp) {
@@ -12,11 +14,9 @@ public:
             return n;
         }
 
-        if (dp[n] != -1) {
-            return dp[n];
+        for(int i = 3 ; i < n ; i++){
+            dp[i] = dp[i -1] + dp[i -2];
         }
-
-        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-        return dp[n];
+        return dp[n -1];
     }
 };
