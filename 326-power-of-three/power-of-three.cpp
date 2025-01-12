fb33cb665e91fef5652eadfb76f84482eee9e6dd
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-01-12 18:37:36 +0530

    Time: 7 ms (26.38%) | Memory: 8.8 MB (60.68%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0 || n % 3 != 0 && n != 1) return false;
+
+        if (n == 1) return true;
+
+        return isPowerOfThree(n/3);
+    }
+};
