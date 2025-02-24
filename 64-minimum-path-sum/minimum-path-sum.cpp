bfd927bcd1039e3d4163591fff3e5f0f00d51afb
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-02-25 00:08:56 +0530

    Time: 4 ms (16.24%) | Memory: 16.4 MB (37.55%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,11 +1,26 @@
 class Solution {
 private:
     int cc(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp, int m, int n) {
-        if (i >= m || j >= n) return INT_MAX;
-        if (i == m - 1 && j == n - 1) return grid[i][j]; 
+        if(i == m - 1 && j == n - 1){
+            return grid[i][j];
+        }
 
-        if (dp[i][j] != -1) return dp[i][j]; 
-        return dp[i][j] = grid[i][j] + min(cc(i + 1, j, grid, dp, m, n), cc(i, j + 1, grid, dp, m, n));
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int path = grid[i][j];
+        int down = INT_MAX, right = INT_MAX;
+
+        if(i + 1 < m){
+            down = cc(i + 1, j, grid, dp, m, n);
+        }
+
+        if(j + 1 < n){
+            right = cc(i, j + 1, grid, dp, m, n);
+        }
+
+        path = grid[i][j] + min(down,right);
+        dp[i][j] = path;
+        return dp[i][j];
     }
 public:
     int minPathSum(vector<vector<int>>& grid) {
