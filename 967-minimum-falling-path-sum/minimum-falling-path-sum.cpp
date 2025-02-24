7bab16fc23187fdc7ac882b4dc16e443de054148
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-02-24 23:47:45 +0530

    Time: 7 ms (8.67%) | Memory: 16.3 MB (5.39%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    int cc(int i, int j, vector<vector<int>> &matrix, int m, int n, vector<vector<int>> &dp){
+        if(i == m - 1){
+            return matrix[i][j];
+        }
+
+        if(dp[i][j] != 1e9) return dp[i][j];
+
+        int path = matrix[i][j];
+        int left = INT_MAX, down = INT_MAX, right = INT_MAX;
+
+        if(i + 1 < m && j - 1 >= 0){
+            left = cc(i + 1, j - 1, matrix, m, n, dp);
+        }
+
+        if(i + 1 < m){
+            down = cc(i + 1, j, matrix, m, n, dp);
+        }
+
+        if(i + 1 < m && j + 1 < n){
+            right = cc(i + 1, j + 1, matrix, m, n, dp);
+        }
+
+        dp[i][j] = path + min({left, down, right});
+        return dp[i][j];
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, 1e9));
+        int minSum = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            minSum = min(minSum, cc(0, j, matrix, m, n, dp)); // for every col
+        }
+
+        return minSum;
+
+
+    }
+};
