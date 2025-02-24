1190805da168af31cd2a4cdbabe91286c16174fb
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-02-25 00:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (56.92%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -29,3 +29,23 @@ public:
         return cc(0, 0, grid, dp, m, n);
     }
 };
+
+
+/*class Solution {
+private:
+    int cc(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp, int m, int n) {
+        if (i >= m || j >= n) return INT_MAX;
+        if (i == m - 1 && j == n - 1) return grid[i][j]; 
+
+        if (dp[i][j] != -1) return dp[i][j]; 
+        return dp[i][j] = grid[i][j] + min(cc(i + 1, j, grid, dp, m, n), cc(i, j + 1, grid, dp, m, n));
+    }
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1)); 
+        return cc(0, 0, grid, dp, m, n);
+    }
+};
+
+*/
