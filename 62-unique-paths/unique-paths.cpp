e9e66ac4603c2d3f8c8bef298c295f184afbd9af
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-02-24 22:15:42 +0530

    Time: 2 ms (18.37%) | Memory: 9.5 MB (8.97%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,32 @@
 class Solution {
 private:
-    int cc(int i, int j, int m, int n, vector<vector<int>> &dp){
+    int cc(int i, int j, int m, int n, int &count, vector<vector<int>> &dp){
         if(i == m - 1 && j == n - 1){
             return 1;
         }
 
-        if(i > m || j > n) return 0;
-
         if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = cc(i + 1, j, m, n, dp) + cc(i, j + 1, m, n, dp);
+        int path = 0;
+
+        if (i + 1 < m){
+            path += cc(i + 1, j , m , n, count, dp);
+        }
+
+        if(j + 1 < n){
+            path += cc(i, j + 1, m, n, count, dp);
+        }
+
+        dp[i][j] = path;
+        return dp[i][j];
+
+
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return cc(0,0,m,n, dp);
+        vector<vector<int>> dp(m, vector<int> (n , -1));
+        int count = 0;
+        return cc(0,0,m,n,count, dp);
+        
     }
 };
