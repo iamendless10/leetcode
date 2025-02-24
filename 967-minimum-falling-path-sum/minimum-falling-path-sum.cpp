215a56ae257139d3af77584732f83c6fc60135af
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-02-24 23:58:52 +0530

    Time: 7 ms (8.67%) | Memory: 16.4 MB (5.39%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,42 +1,25 @@
 class Solution {
 private:
     int cc(int i, int j, vector<vector<int>> &matrix, int m, int n, vector<vector<int>> &dp){
+        if (j < 0 || j >= n) return 1e9;
+
         if(i == m - 1){
             return matrix[i][j];
         }
 
         if(dp[i][j] != 1e9) return dp[i][j];
 
-        int path = matrix[i][j];
-        int left = INT_MAX, down = INT_MAX, right = INT_MAX;
-
-        if(i + 1 < m && j - 1 >= 0){
-            left = cc(i + 1, j - 1, matrix, m, n, dp);
-        }
-
-        if(i + 1 < m){
-            down = cc(i + 1, j, matrix, m, n, dp);
-        }
-
-        if(i + 1 < m && j + 1 < n){
-            right = cc(i + 1, j + 1, matrix, m, n, dp);
-        }
-
-        dp[i][j] = path + min({left, down, right});
-        return dp[i][j];
+        return dp[i][j] = matrix[i][j] + min({cc(i + 1, j - 1, matrix, m, n, dp),cc(i + 1, j, matrix, m, n,dp),cc(i + 1, j + 1, matrix, m, n, dp)});
     }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int m = matrix.size(), n = matrix[0].size();
+        int mini = INT_MAX;
         vector<vector<int>> dp(m, vector<int> (n, 1e9));
-        int minSum = INT_MAX;
-        for (int j = 0; j < n; j++) {
-            minSum = min(minSum, cc(0, j, matrix, m, n, dp)); // for every col
+        for(int j = 0; j < n ; j++){
+            mini = min(mini, cc(0, j, matrix, m, n, dp));
         }
 
-        return minSum;
-
-
+        return mini;
     }
 };
