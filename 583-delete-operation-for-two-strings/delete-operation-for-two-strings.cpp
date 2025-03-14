b81ca1f85f03bf1ed93c8a6b962f23a471555a97
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-03-14 22:48:41 +0530

    Time: 13 ms (45.79%) | Memory: 16.8 MB (58.44%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,21 +1,21 @@
 class Solution {
+private:
+    int cc(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+        if(i < 0 || j < 0)  return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i]==t[j]) return dp[i][j] = 1 + cc(i - 1, j - 1, s, t, dp);
+
+        return dp[i][j] = max(cc(i - 1, j, s, t, dp),cc(i, j - 1, s, t, dp));
+
+    }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n + 1,vector<int> (m + 1, 0));
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m ; j++){
-                if(word1[i - 1] == word2[j - 1]){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else{
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-
-        return (n - dp[n][m]) + (m - dp[n][m]);
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        cc(n - 1, m - 1, word1, word2, dp);
+        return ((n - dp[n - 1][m - 1]) + (m - dp[n - 1][m - 1]));
     }
 };
