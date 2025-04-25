9cd30232166a8c8a757af7296a881ef21c2faff3
Kayal Ennian A G <114800605+iamendless10@users.noreply.github.com>
2025-04-25 18:08:06 +0530

    Time: 3 ms (41.53%) | Memory: 17.8 MB (61.09%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:    
+        visit = set()
+        
+        def get_next_number(n):    
+            output = 0
+            
+            while n:
+                digit = n % 10
+                output += digit ** 2
+                n = n // 10
+            
+            return output
+
+        while n not in visit:
+            visit.add(n)
+            n = get_next_number(n)
+            if n == 1:
+                return True
+        
+        return False
